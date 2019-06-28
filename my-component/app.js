3ddaf83c125433edc06cd1eca7ba30cba0dab277
practice2008 <1814840992@qq.com>
2019-06-28 10:23:50 +0800

    引入colorUi布局 首页显示各个组件的列表 直观的提供展示功能

--- my-component/app.js
+++ my-component/app.js
@@ -1,39 +1,46 @@
 //app.js
 App({
   onLaunch: function () {
-    // 展示本地存储能力
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
-
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
-              }
-            }
-          })
-        }
-      }
-    })
+   
+   
   },
   globalData: {
-    userInfo: null
+    userInfo: null,
+    iconList: [{//首页功能菜单
+      icon: 'discoverfill',
+      color: 'cyan',
+      badge: 0,
+      name: '日报',
+      url: "/pages/daily/index",
+      code: 'daily',
+      power: 'production.procedure.header'
+    },
+    {
+      icon: 'noticefill',
+      color: 'olive',
+      badge: 0,
+      name: '质检',
+      url: "/pages/quality-test/index",
+      code: 'quality',
+      power: ''
+    }, {
+      icon: 'icloading',
+      color: 'cyan',
+      badge: 0,
+      name: '进度',
+      url: "/pages/progress/index",
+      code: 'progress',
+      power: ''
+    }, {
+      icon: 'rankfill',
+      color: 'brown',
+      badge: 0,
+      name: '库存',
+      code: 'stock',
+      url: "/pages/stock/index",
+      code: 'stock',
+      power: ''
+    }
+    ]
   }
 })
--- my-component/pages/my-popup/index.js
+++ my-component/pages/my-popup/index.js
@@ -11,18 +11,25 @@ Page({
   },
   showPopup() {//显示弹窗框
     Popup.alert({
-      selector:'#my-Popups',
+      selector: '#my-Popups',//绑定的id选择器
       title: '提示',
       content: '今天从上海到北京，走了三百公里,哈哈哈哈哈哈',
-      type:'String'
-    }).then((res) => {
-      console.log(111)
-    }).catch((res) => {
-      console.log(222)
+      type: 'String'
     });
+
+    // Popup.alert({
+    //   selector:'#my-Popups',
+    //   title: '提示',
+    //   content: '今天从上海到北京，走了三百公里,哈哈哈哈哈哈',
+    //   type:'String'
+    // }).then((res) => {
+    //   console.log(111)
+    // }).catch((res) => {
+    //   console.log(222)
+    // });
   },
   _onConfirm(){//确认的点击回调事件
     Popup.close({selector: '#my-Popups'});
-    console.log(8877);
+    console.log("回调方法");
   }
 })
